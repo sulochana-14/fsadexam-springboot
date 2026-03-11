83d55e6b7f9706d443c396305a9b9e3122caebea
sulochana-14 <rajasulochana2007@gmail.com>
2026-03-11 12:01:43 +0530

    Initial FSAD Maven Hibernate Project

---
+++ fsadexam/src/main/java/com/klef/fsad/fsadexam/FsadexamApplication.java
@@ -0,0 +1,13 @@
+package com.klef.fsad.fsadexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsadexamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsadexamApplication.class, args);
+	}
+
+}
---
+++ fsadexam/src/test/java/com/klef/fsad/fsadexam/FsadexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klef.fsad.fsadexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsadexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
